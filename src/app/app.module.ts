7887eb1f51c0723b06b7e57fd6246ef91d6b6539
Alfonso Chang <alfonsochang@MacBook-Pro-de-Alfonso.local>
2023-05-01 13:29:20 -0600

    Iniciando Proyecto TRANSPORTE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/acceso/login/login.component';
+import { CambioclaveComponent } from './components/acceso/cambioclave/cambioclave.component';
+import { HeaderComponent } from './components/menu/header/header.component';
+import { SidenavComponent } from './components/menu/sidenav/sidenav.component';
+import { HomeComponent } from './components/menu/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CambioclaveComponent,
+    HeaderComponent,
+    SidenavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
